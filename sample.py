1cca42d650a17584081b3c3049846a7023485cf4
gautamsi <gautamsi@in.ibm.com>
2018-05-18 15:33:03 +0530

    save images rather than showing plots.

--- sample.py
+++ sample.py
@@ -14,6 +14,6 @@ model = VRNN(x_dim, h_dim, z_dim, n_layers)
 model.load_state_dict(state_dict)
 
 sample = model.sample(28*6)
-plt.imsave('sample_sample.py', sample.numpy(), cmap='gray')
+plt.imsave('sample_sample.png', sample.numpy(), cmap='gray')
 # plt.imshow(sample.numpy(), cmap='gray')
 # plt.show()
